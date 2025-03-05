ce412cce47cee4fdac73a1e23e13e97437e3b5b3
NK Neyshan <neyshankhondoker@gmail.com>
2025-03-05 17:52:12 +0600

    fix the logic of calculating the sum of even numbers

--- 03_loops/02_solution.py
+++ 03_loops/02_solution.py
@@ -1,8 +1,8 @@
-n = 10
+n = int(input("Enter the number to add up to: "))  # takes the limit from user
 sum_even = 0
 
 for i in range(1, n+1):
     if i%2 == 0:
-        sum_even += 1
+        sum_even += i   # adds the current even number to the sum of the previous ones
 
-print("Sum of even number is: , ", sum_even)
+print(f"Sum of the even numbers upto {n} is {sum_even}")
